b44563dd3a8125e135e7e9ac4206ec9ea0ebacf3
jsilman <jrsilman@gmail.com>
2020-11-27 23:29:41 -0500

    modified:   codesample.sh 	new file:   codesample.sh.bak

--- codesample.sh
+++ codesample.sh
@@ -25,57 +25,27 @@ all_dbs () {
 }
 
 tables () {
-	if [ "$REPLY" = 2 ] && [ "$result" = 0 ]; then
-		read -p "You selected $REPLY which is $opt. Please enter database name: " answer
-		echo $answer
-		$mybin -e 'SELECT table_name AS "Table", round(((data_length + index_length) / 1024 / 1024), 2) as "size (MB)" FROM information_schema.TABLES WHERE table_schema = "$answer"  ORDER BY data_length DESC;'
+if [ "$REPLY" = 2 ] && [ "$result" = 0 ]; then
+	read -p "You selected $REPLY which is $opt. Please enter database name: " answer
+	echo $answer
+	$mybin -e "SELECT table_name AS 'Table', round(((data_length + index_length) / 1024 / 1024), 2) as 'size (MB)' FROM information_schema.TABLES WHERE table_schema = '${answer}' ORDER BY data_length DESC;"
 
-	else [ "$REPLY" = "2" ] && [ "$result" = 1 ]
-		echo "MySQL is not present on this system. Goodbye!"
-		exit
-	fi
+else [ "$REPLY" = "2" ] && [ "$result" = 1 ]
+	echo "MySQL is not present on this system. Goodbye!"
+	exit
+fi
 }
 
 while true; do
 
-#select opt in 'All Databases' 'Tables in a Database' Quit; do
 	options=("All Databases" "Tables in a Database" "Quit")
 
 	select opt in "${options[@]}"; do
 		case $REPLY in
 			1) all_dbs;;
-
 			2) tables;;
-	#			echo "You selected $opt"
 			3) echo "Goodbye!"; exit;;
 			*) echo "Invalid option $REPLY";;
 		esac
 	done
-
-#if [ "$REPLY" = "1" ] && [ "$result" = 0 ]; then
-#	$mybin -e 'SELECT table_schema "DB Name",
-#sum( data_length + index_length ) / 1024 /
-#1024 "DB Size in MB",
-#sum( data_free )/ 1024 / 1024 "Free Space in MB"
-#FROM information_schema.TABLES
-#GROUP BY table_schema ;'
-
-#elif [ "$REPLY" = "1" ] && [ "$result" = 1 ]; then
-#	echo "MySQL is not present on this system. Goodbye!"
-#	break
-
-#elif [ "$REPLY" = 2 ] && [ "$result" = 0 ]; then
-#	read -p "You selected $REPLY which is $opt. Please enter database name: " answer
-#	echo $answer
-#	$mybin -e 'SELECT table_name AS "Table", round(((data_length + index_length) / 1024 / 1024), 2) as "size (MB)" FROM information_schema.TABLES WHERE table_schema = "$answer"  ORDER BY data_length DESC;'
-
-#elif [ "$REPLY" = "2" ] && [ "$result" = 1 ]; then
-#	echo "MySQL is not present on this system. Goodbye!"
-#	break
-
-#elif [ "$REPLY" = "3" ]; then
-#	echo "Goodbye!"
-#	break
-#fi
 done
-
